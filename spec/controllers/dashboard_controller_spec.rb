9351f29a982e7e77f9caac5de3c5d9173f47bec8
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-23 15:48:53 -0600

    fixed dashboard spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe DashboardController, type: :controller do
 
   describe "GET #home" do
-    it "returns http success" do
+    it "returns http redirect 302" do
       get :home
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
