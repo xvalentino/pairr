bea498c48f3bff0f6dff91f9de265747439ad2b4
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-23 14:10:54 -0600

    I have a login feature spec now cool

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Login", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it "works" do
+    github_login
+    root_path
+    github_login
+    visit root_path
+    click_link_or_button("Login")
+    expect(page).to_have_content "Hello"
+  end
 end
