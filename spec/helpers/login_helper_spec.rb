a716ec4e11838a143dce055cad6eb876f423e5d9
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-24 10:01:25 -0600

    this is supposed to work? but it's not, oh well

--- spec/helpers/login_helper_spec.rb
+++ spec/helpers/login_helper_spec.rb
@@ -1,1 +1,16 @@
-
+def github_login
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.logger = Rails.logger
+  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({ 
+    provider: "github",
+    uid: "123545",
+    info: {
+      name: "Paul Grever",
+      image: "https://avatars.githubusercontent.com/u/3664281?v=3"
+    },
+    credentials: {
+      token: "mock_token",
+      secret: "mock_secret"
+    }
+  })
+end
