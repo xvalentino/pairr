750792695fd8db14e930b3f96846826c171d7c6e
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-24 04:03:38 -0600

    have route and link to go to match page, also there is a test testing that one user is show another users information

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   get 'login', to: 'sessions#show'
   get '/auth/github/callback', to: 'sessions#create'
+  get '/match', to: 'match#show'
 
   resources :users, only: [:edit, :update]
 end
